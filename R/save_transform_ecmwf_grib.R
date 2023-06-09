bf7c70444413f97138ffdb201df9e1b9f07d534e
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2023-06-09 11:54:49 -0400

    full ecmwf and sentinel transforms

--- R/save_transform_ecmwf_grib.R
+++ R/save_transform_ecmwf_grib.R
@@ -3,30 +3,32 @@
 #' .. content for \details{} ..
 #'
 #' @title
-#' @param ecmwf_forecasts_download
-#' @param directory
+#' @param ecmwf_forecasts_downloaded
+#' @param transform_directory
+#' @param verbose
 #' @return
 #' @author Emma Mendelsohn
 #' @export
-preprocess_ecmwf_forecasts <- function(ecmwf_forecasts_download,
-                                       preprocessed_directory,
-                                       n_workers = NULL) {
+save_transform_ecmwf_grib <- function(ecmwf_forecasts_downloaded,
+                                      transform_directory =
+                                      paste0(str_replace(ecmwf_forecasts_directory,
+                                      "gribs", "flat"), "_transformed"),
+                                      verbose = TRUE) {
   
-  suppressWarnings(dir.create(here::here(preprocessed_directory), recursive = TRUE))
-  existing_files <- list.files(preprocessed_directory)
+  if(verbose) cat(ecmwf_forecasts_downloaded, "\n")
+  
+  suppressWarnings(dir.create(here::here(transform_directory), recursive = TRUE))
+  existing_files <- list.files(transform_directory)
   
   # filename for postprocessed file
-  filename <- str_replace(basename(ecmwf_forecasts_download), "\\.grib", "\\.gz.parquet")
-  
-  # begin processing
-  message(paste0("Preprocessing ", ecmwf_forecasts_download))
+   filename <- str_replace(basename(ecmwf_forecasts_downloaded), "\\.grib", "\\.gz.parquet")
   
   if(filename %in% existing_files){
     message("file already exists, skipping preprocess")
-    return(file.path(preprocessed_directory, filename)) # skip if file exists
+    return(file.path(transform_directory, filename)) # skip if file exists
   }
   
-  file <- here::here(ecmwf_forecasts_download)
+  file <- here::here(ecmwf_forecasts_downloaded)
   
   # read in with terra
   grib <- terra::rast(file)
@@ -43,7 +45,7 @@ preprocess_ecmwf_forecasts <- function(ecmwf_forecasts_download,
     mutate(variable_id = as.character(glue::glue("{data_date}_step{step_range}_{data_type}_{short_name}"))) |> 
     mutate(data_date = ymd(data_date))  |> 
     select(-grid_type, -packing_type, -level, -type_of_level, -centre, -edition)
-
+  
   # Calculate per-pixel mean and std for each unique combination, across all models
   grib_index <- as.integer(factor(meta$variable_id, levels = unique(meta$variable_id)))
   grib_means <- terra::tapp(grib, grib_index, "mean", cores = n_workers) |> 
@@ -58,13 +60,14 @@ preprocess_ecmwf_forecasts <- function(ecmwf_forecasts_download,
   
   # Link with metadata for export
   dat_out <- grib_means |> 
-    mutate(std = grib_stds) |> 
+    mutate(std = grib_sds) |> 
     left_join(distinct(meta), by = "variable_id") |> 
     arrange(variable_id, x, y) |> 
     select(x,y, mean, std, data_date, step_range, data_type, short_name)
   
-  write_parquet(dat_out, here::here(preprocessed_directory, filename), compression = "gzip", compression_level = 5)
+  write_parquet(dat_out, here::here(transform_directory, filename), compression = "gzip", compression_level = 5)
   
-  return(file.path(preprocessed_directory, filename))
-  
+  return(file.path(transform_directory, filename))
+
+
 }
--- _targets.R
+++ _targets.R
@@ -30,9 +30,8 @@ static_targets <- tar_plan(
   tar_target(continent_polygon, create_africa_polygon()),
   tar_target(continent_bounding_box, sf::st_bbox(continent_polygon)),
   tar_target(continent_raster_template,
-            wrap(terra::rast(ext(continent_polygon), 
-                             #crs = "+proj=aea +lat_0=0 +lon_0=24 +lat_1=-24 +lat_2=-33 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs",
-                             resolution = 0.1))),
+             wrap(terra::rast(ext(continent_polygon), 
+                              resolution = 0.1))),
   tar_target(continent_raster_template_plot, create_raster_template_plot(rast(continent_raster_template), continent_polygon))
   
 )
@@ -97,7 +96,7 @@ dynamic_targets <- tar_plan(
                   prefix = "open-rvfcast/",
                   check = TRUE)}, 
     cue = tar_cue("thorough")), 
-
+  
   
   # NASA POWER recorded weather -----------------------------------------------------------
   
@@ -166,43 +165,47 @@ dynamic_targets <- tar_plan(
 data_targets <- tar_plan(
   
   # Transform Sentinel NDVI
-  # save locally as files
-  # TODO diff between template and raster crs
-  tar_target(sentinel_ndvi_rasters_transformed, 
+  tar_target(sentinel_ndvi_transformed_rasters, 
              save_transform_raster(raster_file = sentinel_ndvi_downloaded, 
                                    template = continent_raster_template,
                                    transform_directory = paste0(sentinel_ndvi_directory, "_transformed"),
                                    verbose = TRUE),
-             pattern = head(sentinel_ndvi_downloaded, 4), 
+             pattern = sentinel_ndvi_downloaded, 
              format = "file", 
              repository = "local",
              cue = tar_cue("thorough")),  
   
   # TODO what are the units (differs between sentinel and modis)
-  # TODO this needs to handle the batching from modis
-  tar_target(modis_ndvi_rasters_transformed, 
-             save_transform_raster(raster_file = modis_ndvi_downloaded, 
-                                   template = continent_raster_template,
-                                   transform_directory = paste0(modis_ndvi_directory, "_transformed"),
-                                   verbose = TRUE),
-             pattern = head(modis_ndvi_downloaded, 4), 
+  # TODO this needs to handle the internal batching from modis (tar_rep?)
+  # tar_target(modis_ndvi_transformed_rasters,
+  #            save_transform_raster(raster_file = modis_ndvi_downloaded, 
+  #                                  template = continent_raster_template,
+  #                                  transform_directory = paste0(modis_ndvi_directory, "_transformed"),
+  #                                  verbose = TRUE),
+  #            pattern = head(modis_ndvi_downloaded, 4), 
+  #            format = "file", 
+  #            repository = "local",
+  #            cue = tar_cue("thorough")), 
+  
+  # Transform ECMWF
+  tar_target(ecmwf_forecasts_flat_transformed,
+             save_transform_ecmwf_grib(ecmwf_forecasts_downloaded,
+                                       transform_directory = paste0(str_replace(ecmwf_forecasts_directory, "gribs", "flat"), "_transformed"),
+                                       verbose = TRUE),
+             pattern = map(ecmwf_forecasts_downloaded),
+             iteration = "list",
              format = "file", 
              repository = "local",
-             cue = tar_cue("thorough")), 
-  
-  # convert ecmwf to raster
-  
-  # convert terra power to raster
-  
-  # tar_target(ecmwf_forecasts_preprocessed,
-  #            preprocess_ecmwf_forecasts(ecmwf_forecasts_download,
-  #                                       preprocessed_directory =  "data/ecmwf_parquets"),
-  #            pattern = map(ecmwf_forecasts_download), 
-  #            iteration = "list",
-  #            format = "file" 
-  # ),
+             cue = tar_cue("thorough")),  
   
 
+
+  
+
+  #  terra power 
+  
+  
+  
   # merge data together
   
 )
