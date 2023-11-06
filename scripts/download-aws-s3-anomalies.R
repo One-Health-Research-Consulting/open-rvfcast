917baeb61eb7a67c0d3bd28b8ed4bd49387fc92d
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2023-11-06 16:05:31 -0500

    add ndvi and forecasts to the anomaly download script

--- scripts/download-aws-s3-anomalies.R
+++ scripts/download-aws-s3-anomalies.R
@@ -1,7 +1,6 @@
 suppressPackageStartupMessages(source("packages.R"))
 aws_bucket <- Sys.getenv("AWS_BUCKET_ID")
 
-
 #  Weather ------------------------------------------------------------
 message("downloading weather_anomalies_directory")
 weather_anomalies_directory <- tar_read(weather_anomalies_directory)
@@ -9,4 +8,21 @@ aws_s3_download(path = weather_anomalies_directory,
                 bucket = aws_bucket ,
                 key =  weather_anomalies_directory,
                 check = TRUE)
+
+#  NDVI ------------------------------------------------------------
+message("downloading ndvi_anomalies_directory")
+ndvi_anomalies_directory <- tar_read(ndvi_anomalies_directory)
+aws_s3_download(path = ndvi_anomalies_directory,
+                bucket = aws_bucket ,
+                key =  ndvi_anomalies_directory,
+                check = TRUE)
+
+#  Forecasts ------------------------------------------------------------
+message("downloading forecasts_anomalies_directory")
+forecasts_anomalies_directory <- tar_read(forecasts_anomalies_directory)
+aws_s3_download(path = forecasts_anomalies_directory,
+                bucket = aws_bucket ,
+                key =  forecasts_anomalies_directory,
+                check = TRUE)
+
 
