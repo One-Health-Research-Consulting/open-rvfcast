4f91aa46298724fe3a0d1a39abc758135bd7705e
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2023-12-20 12:32:49 -0500

    add other weather datasets

--- scripts/eda/app.R
+++ scripts/eda/app.R
@@ -34,13 +34,20 @@ pal_ndvi_anomalies <- leaflet::colorNumeric(palette = grDevices::colorRamp(c("#4
                                             na.color = "transparent")
 
 # temp palette
-# v_temperature_anomalies <- arrow::open_dataset(weather_anomalies) |> 
-#     dplyr::select(anomaly_temperature_30, anomaly_temperature_60, anomaly_temperature_90) |> 
-#     dplyr::collect() |>
-#     as.matrix()
+v_weather_anomalies <- arrow::open_dataset(weather_anomalies) |>
+    dplyr::select(anomaly_relative_humidity_30, anomaly_relative_humidity_60, anomaly_relative_humidity_90) |>
+    dplyr::collect() |>
+    as.matrix()
 pal_temperature_anomalies <- leaflet::colorNumeric(palette = grDevices::colorRamp(c("#00008B", "#ADD8E6", "#DDDAC3", "#FFB6C1", "#FF0000"), interpolate = "linear"), 
-                                                   domain = c(-6.4, 0, 6.4),  # hardcode min/max from v_ndvi_anomalies
+                                                   domain = c(-6.4, 0, 6.4),  # hardcode min/max 
                                                    na.color = "transparent")
+pal_precipitation_anomalies <- leaflet::colorNumeric(palette = grDevices::colorRamp(c("#4C392D", "#C4A484", "#DDDAC3", "#ADD8E6","#00008B"), interpolate = "linear"), 
+                                                     domain = c(-10, 0, 10),  # hardcode min/max (limit to 0.999%)
+                                                     na.color = "transparent")
+pal_relative_humidity_anomalies <- leaflet::colorNumeric(palette = grDevices::colorRamp(c("#4C392D", "#C4A484", "#DDDAC3", "#ADD8E6","#00008B"), interpolate = "linear"), 
+                                                         domain = c(-20, 0, 20),  # hardcode min/max from v_ndvi_anomalies
+                                                         na.color = "transparent")
+
 
 # define function to make maps from arrow dataset
 create_arrow_leaflet <- function(conn, field, selected_date, palette, domain, include_legend = FALSE){
@@ -77,22 +84,40 @@ ui <- fluidPage(
   fluidRow(
     column(4, radioButtons("selected_dataset", 
                            "Select Dataset", 
-                           choices = c("NDVI", "Temperature"), inline = TRUE)),
+                           choices = c("NDVI" = "ndvi", 
+                                       "Temperature" = "temperature", 
+                                       "Precipitation" = "precipitation", 
+                                       "Relative Humidity" = "relative_humidity"), inline = TRUE)),
     column(4, shinyWidgets::sliderTextInput("selected_date", 
                                             "Select a Date",
                                             choices = model_dates_selected,
                                             animate = TRUE)), # animationOptions to set faster but data load cant keep up
     conditionalPanel(
-      condition = "input.selected_dataset == 'NDVI'",
+      condition = "input.selected_dataset == 'ndvi'",
       tags$h5(glue::glue("{anamaly_text} 
               Negative values indicate NDVI is more brown / less green than average.
               Positive values indicate NDVI is more green / less brown than average."))
     ),
     conditionalPanel(
-      condition = "input.selected_dataset == 'Temperature'",
+      condition = "input.selected_dataset == 'temperature'",
       tags$h5(glue::glue("{anamaly_text} 
+              Units are in celsius. 
               Negative values indicate temperature is colder than average.
               Positive values indicate temperature is hotter than average."))
+    ),
+    conditionalPanel(
+      condition = "input.selected_dataset == 'precipitation'",
+      tags$h5(glue::glue("{anamaly_text} 
+              Units are in mm/day. 
+              Negative values indicate lower precipitation than average.
+              Positive values indicate higher precipitation than average."))
+    ),
+    conditionalPanel(
+      condition = "input.selected_dataset == 'relative_humidity'",
+      tags$h5(glue::glue("{anamaly_text} 
+              Units are in %.
+              Negative values indicate lower relative humidity than average.
+              Positive values indicate higher relative humidity than average."))
     ),
   ),
   
@@ -121,8 +146,10 @@ server <- function(input, output) {
   
   conn <- reactive({
     files <- switch(input$selected_dataset, 
-                    "NDVI" = ndvi_anomalies,
-                    "Temperature" = weather_anomalies)
+                    "ndvi" = ndvi_anomalies,
+                    "temperature" = weather_anomalies,
+                    "precipitation" = weather_anomalies,
+                    "relative_humidity" = weather_anomalies)
     
     filename <- files[grepl(input$selected_date, files)]
     arrow::open_dataset(filename) 
@@ -130,14 +157,19 @@ server <- function(input, output) {
   
   pal <- reactive({
     switch(input$selected_dataset, 
-           "NDVI" = pal_ndvi_anomalies,
-           "Temperature" = pal_temperature_anomalies)
+           "ndvi" = pal_ndvi_anomalies,
+           "temperature" = pal_temperature_anomalies,
+           "precipitation" = pal_precipitation_anomalies,
+           "relative_humidity" = pal_relative_humidity_anomalies)
+    
   })
   
   dom <- reactive({
     switch(input$selected_dataset, 
-           "NDVI" = c(-0.65, 0, 0.65),
-           "Temperature" = c(-6.4, 0, 6.4))
+           "ndvi" = c(-0.65, 0, 0.65),
+           "temperature" = c(-6.4, 0, 6.4),
+           "precipitation" = c(-10, 0, 10),
+           "relative_humidity" = c(-20, 0, 20))
   })
   
   output$anomalies_map_30 <- renderLeaflet({
