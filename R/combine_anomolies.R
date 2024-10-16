2f8f2cc5c432899bb6362b742535311cd71f837d
Nathan Layman <layman@ecohealthalliance.org>
2024-10-16 12:20:36 -0700

    Update roxygen header for combine_anomolies function

--- R/combine_anomolies.R
+++ R/combine_anomolies.R
@@ -1,3 +1,26 @@
+#' Combine and Save Weather, Forecasts, and NDVI Anomalies
+#'
+#' This function takes in file paths of weather, forecast, and NDVI anomalies, combines them and saves as partitioned Parquets using hive partitioning by date
+#'
+#' @author R Programmer
+#'
+#' @param weather_anomalies File path of the weather anomalies.
+#' @param forecasts_anomalies File path of the forecast anomalies.
+#' @param ndvi_anomalies File path of the NDVI anomalies.
+#' @param combined_anomolies_directory Directory where the processed files will be saved.
+#' @param ... Additional arguments not used by this function but included for generic function compatibility.
+#'
+#' @return A list containing filepaths of the newly created .parquet files
+#'
+#' @note This function requires Apache Arrow library for reading datasets and writing them as parquet files.
+#'
+#' @examples
+#' combine_anomolies('<path_to_weather_anomalies>', 
+#'                   '<path_to_forecasts_anomalies>', 
+#'                   '<path_to_ndvi_anomalies>', 
+#'                   '<path_to_output_directory>')
+#'
+#' @export
 combine_anomolies <- function(weather_anomalies,
                               forecasts_anomalies,
                               ndvi_anomalies,
