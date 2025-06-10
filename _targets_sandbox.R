4207f5ce52209a98a538980784d7103dff0857c1
Nathan Layman <n8.layman@gmail.com>
2025-06-09 21:09:52 -0700

    Fix a bunch of renv issues

--- _targets_sandbox.R
+++ _targets_sandbox.R
@@ -1,9 +1,3 @@
-# Re-record current dependencies for CAPSULE users
-if(Sys.getenv("USE_CAPSULE") %in% c("1", "TRUE", "true"))
-  capsule::capshot(c("packages.R",
-                     list.files(pattern = "_targets.*\\.(r|R)$", full.names = TRUE),
-                     list.files("R", pattern = "\\.(R|r)$", full.names = TRUE)))
-
 # Load packages (in packages.R) and load project-specific functions in R folder
 suppressPackageStartupMessages(source("packages.R"))
 for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
