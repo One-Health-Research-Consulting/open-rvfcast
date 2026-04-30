7c3758bb591ab8041477f20aedfa7c4840189c84
Morgan Kain <morgankain@eha-dt-10976-kain.mynetworksettings.com>
2026-04-30 18:13:16 -0400

    some code cleanup, mostly to support a move to the native pipe

--- R/mask_and_cluster.R
+++ R/mask_and_cluster.R
@@ -21,116 +21,127 @@ mask_and_cluster_build_template <- function(
   , district_id_col = "shapeName"
   , out_dir
 ) {
-  
+
   ## Check that we're only working on one date at a time
   stopifnot(length(cov_files) == 1)
-  
+
   ## Read one parquet file
-  df <- read_parquet(cov_files) %>% 
-    filter(forecast_interval == 30) %>%
-    dplyr::select(-forecast_interval) %>%
-    mutate(index = seq(n()), .before = 1)
-  
+  df <- read_parquet(cov_files) |>
+    filter(forecast_interval == 30) |>
+    dplyr::select(-forecast_interval) |>
+    mutate(index = seq_len(n()), .before = 1)
+
   ## NOTE: Quite possibly a better way to do this, but this is what we are doing for now...
-  
+
   ## ** First for country and ADM2 region ------------------------------------------------
-  
+
   ## Ensure districts are in projected CRS (ensure valid polygons)
   countries_sf <- lapply(countries_sf, FUN = function(x) {
-    sf::st_make_valid(x) %>% st_collection_extract("POLYGON")
-  }) 
-  
+    sf::st_make_valid(x) |> st_collection_extract("POLYGON")
+  })
+
   crs_target   <- lapply(countries_sf, FUN = function(x) sf::st_crs(x))
-  
+
   ## Extract all of the identifier names for the sub-sub regions, retaining just the name column
   all_identifiers <- lapply(countries_sf, FUN = function(x) {
     district_identifiers <- names(x)
-    district_name_col    <- district_identifiers[grepl("Name", district_identifiers)]
+    district_name_col   <- district_identifiers[grepl("Name", district_identifiers)]
     district_identifiers <- district_identifiers[!grepl("Name", district_identifiers)]
-    return(list(district_name_col, district_identifiers))
+    list(district_name_col, district_identifiers)
   })
-  
+
   names(all_identifiers) <- lapply(countries_sf, FUN = function(x) x$shapeGroup[1])
-  
+
   df_sorted <- purrr::pmap(list(crs_target, countries_sf, all_identifiers, names(all_identifiers)), .f = function(x, y, z, q) {
     ## Convert to sf
     pts_sf <- sf::st_as_sf(df, coords = c("x", "y"), crs = x)
-    
+
     old_s2 <- sf_use_s2(TRUE)
-    
+
     ## Spatial join: keep only points in the full set of sub-sub regions
-    joined <- try({sf::st_join(pts_sf, y, left = FALSE)}, silent = T)
-    
+    joined <- try({
+        sf::st_join(pts_sf, y, left = FALSE)
+    }, silent = TRUE)
+
     if (class(joined)[1] == "try-error") {
       old_s2 <- sf_use_s2(FALSE)
-      joined <- try({sf::st_join(pts_sf, y, left = FALSE)}, silent = T)
+      joined <- try({
+        sf::st_join(pts_sf, y, left = FALSE)
+      }, silent = TRUE)
     }
-    
+
     ## Extract coordinates back into x and y columns
     ## (mostly for plotting and debugging purposes)
     coords <- sf::st_coordinates(joined)
-    joined <- joined %>% 
-      mutate(x = coords[, 1], y = coords[, 2], .before = 1) %>%
-      as_tibble() %>%
-      dplyr::select(-all_of(z[[2]])) %>%
-      relocate(!!z[[1]], .after = "y") %>%
-      mutate(Country = q, .before = shapeName) %>%
+    joined <- joined |>
+      mutate(x = coords[, 1], y = coords[, 2], .before = 1) |>
+      as_tibble() |>
+      dplyr::select(-all_of(z[[2]])) |>
+      relocate(!!z[[1]], .after = "y") |>
+      mutate(Country = q, .before = shapeName) |>
       relocate(index, .before = 1)
-    
-  }) %>% do.call("rbind", .)
-  
+
+  }) |>
+  bind_rows()
+
   ## ** Then for hex ------------------------------------------------
   hex_sf <- lapply(hex_sf, FUN = function(x) {
-    sf::st_make_valid(x) %>% st_collection_extract("POLYGON")
-  }) 
-  
+    sf::st_make_valid(x) |> st_collection_extract("POLYGON")
+  })
+
   crs_target   <- lapply(hex_sf, FUN = function(x) sf::st_crs(x))
-  
+
   ## Extract all of the identifier names for the sub-sub regions, retaining just the name column
   all_identifiers <- lapply(hex_sf, FUN = function(x) {
     district_identifiers <- names(x)
-    district_name_col    <- district_identifiers[grepl("Name", district_identifiers)]
+    district_name_col   <- district_identifiers[grepl("Name", district_identifiers)]
     district_identifiers <- district_identifiers[!grepl("Name", district_identifiers)]
-    return(list(district_name_col, district_identifiers))
+    list(district_name_col, district_identifiers)
   })
-  
+
   names(all_identifiers) <- lapply(hex_sf, FUN = function(x) x$shapeGroup[1])
-  
+
   df_sorted2 <- purrr::pmap(list(crs_target, hex_sf, all_identifiers, names(all_identifiers)), .f = function(x, y, z, q) {
     ## Convert to sf
     pts_sf <- sf::st_as_sf(df, coords = c("x", "y"), crs = x)
-    
+
     old_s2 <- sf_use_s2(TRUE)
-    
+
     ## Spatial join: keep only points in the full set of sub-sub regions
-    joined <- try({sf::st_join(pts_sf, y, left = FALSE)}, silent = T)
-    
+    joined <- try({
+        sf::st_join(pts_sf, y, left = FALSE)
+    }, silent = TRUE)
+
     if (class(joined)[1] == "try-error") {
       old_s2 <- sf_use_s2(FALSE)
-      joined <- try({sf::st_join(pts_sf, y, left = FALSE)}, silent = T)
+      joined <- try({
+        sf::st_join(pts_sf, y, left = FALSE)
+      }, silent = TRUE)
     }
-    
+
     ## Extract coordinates back into x and y columns
     ## (mostly for plotting and debugging purposes)
     coords <- sf::st_coordinates(joined)
-    joined <- joined %>% 
-      mutate(x = coords[, 1], y = coords[, 2], .before = 1) %>%
-      as_tibble() %>%
-      dplyr::select(-all_of(z[[2]])) %>%
-      relocate(!!z[[1]], .after = "y") %>%
+    joined <- joined |>
+      mutate(x = coords[, 1], y = coords[, 2], .before = 1) |>
+      as_tibble() |>
+      dplyr::select(-all_of(z[[2]])) |>
+      relocate(!!z[[1]], .after = "y") |>
       mutate(Country = q, .before = shapeName)
-    
-  }) %>% do.call("rbind", .)
-  df_sorted2 <- df_sorted2 %>% dplyr::select(index, shapeName)
-  
-  df_sorted <- df_sorted %>% 
-    rename(ADM2 = shapeName) %>% 
-    left_join(., df_sorted2) %>%
+
+  }) |>
+  bind_rows()
+
+  df_sorted2 <- df_sorted2 |> dplyr::select(index, shapeName)
+
+  df_sorted <- df_sorted |>
+    rename(ADM2 = shapeName) |>
+    left_join(df_sorted2) |>
     relocate(shapeName, .after = ADM2)
-  
-  
-  return(df_sorted %>% dplyr::select(x, y, Country, ADM2, shapeName))
-  
+
+
+  df_sorted |> dplyr::select(x, y, Country, ADM2, shapeName)
+
 }
 
 
@@ -148,45 +159,45 @@ mask_and_cluster_from_template <- function(template, cov_files, out_dir, overwri
 
   ## Check that we're only working on one date at a time
   stopifnot(length(cov_files) == 1)
-  
+
   ## Set filename
   save_filename <- paste(
     out_dir
     , "/"
-    , out_dir %>% strsplit("data/") %>% unlist() %>% pluck(length(.))
+    , out_dir |> strsplit("data/") |> unlist() |> pluck(length(.))
     , "_"
-    , cov_files %>% strsplit("data_") %>% unlist() %>% pluck(length(.))
+    , cov_files |> strsplit("data_") |> unlist() |> pluck(length(.))
     , sep = ""
   )
   message(paste0("Processing ", cov_files))
-  
+
   ## Check if file already exists and can be read
   error_safe_read_parquet <- possibly(arrow::open_dataset, NULL)
-  existing_dataset <- error_safe_read_parquet(save_filename)
+  existing_dataset        <- error_safe_read_parquet(save_filename)
 
-  if(!is.null(existing_dataset) & !overwrite) {
+  if (!is.null(existing_dataset) && !overwrite) {
     # Check if file has data - if zero rows, overwrite anyway
     row_count <- existing_dataset |> count() |> collect() |> pull(n)
-    if(row_count > 0) {
+    if (row_count > 0) {
       message("file already exists and can be loaded, skipping processing")
       return(save_filename)
     } else {
       message("file exists but has zero rows, overwriting")
     }
   }
-  
+
   ## Read one parquet file
   df <- read_parquet(cov_files)
-  
+
   ## Join with the template to get the ADM region info
-  df_with_adm <- df %>% 
-    left_join(., template) %>% 
-    relocate(Country, ADM2, shapeName, .after = y) %>%
+  df_with_adm <- df |>
+    left_join(template) |>
+    relocate(Country, ADM2, shapeName, .after = y) |>
     filter(!is.na(Country))
-  
+
   ## Write output to a parquet file
   arrow::write_parquet(df_with_adm, save_filename, compression = "gzip", compression_level = 5)
-  
-  return(save_filename)
-  
+
+  save_filename
+
 }
